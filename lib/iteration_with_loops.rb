fcdab34da846e6ddc1b997a16688bc23fbea7fbd
Boonie Attawut <puifai.ba@gmail.com>
2020-12-16 23:12:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,8 @@ def find_min_in_nested_arrays(src)
       min = 0
       j = 0
         while j < src[i].count do
-          if src[i][j] < min
-             min = min + src[i][j]
+          if src[i][j] <= min
+             min = src[i][j]
           end
         j += 1
         end
